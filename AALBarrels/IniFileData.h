118449e272b635b0330074cb131439e973832cd4
benkopolis <benkopolis@102d4010-3c10-11de-be05-5f7a86268029>
2009-05-28 17:01:13 +0000

    Klasa która ładuje dane z pliku ini - deklaracje

---
+++ AALBarrels/IniFileData.h
@@ -0,0 +1,23 @@
+/*
+ * IniFileData.h
+ *
+ *  Created on: 2009-05-28
+ *      Author: zby
+ */
+
+#ifndef INIFILEDATA_H_
+#define INIFILEDATA_H_
+
+namespace AALBarrels
+{
+
+class IniFileData
+{
+public:
+	IniFileData();
+	virtual ~IniFileData();
+};
+
+}
+
+#endif /* INIFILEDATA_H_ */
