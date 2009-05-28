53af628e9ca93a7891cb723209a658247475465c
benkopolis <benkopolis@102d4010-3c10-11de-be05-5f7a86268029>
2009-05-28 17:01:40 +0000

    Klasa która ładuje dane z pliku ini - definicje

---
+++ trunk/AALBarrels/IniFileData.cpp
@@ -0,0 +1,24 @@
+/*
+ * IniFileData.cpp
+ *
+ *  Created on: 2009-05-28
+ *      Author: zby
+ */
+
+#include "IniFileData.h"
+
+namespace AALBarrels
+{
+
+IniFileData::IniFileData()
+{
+	// TODO Auto-generated constructor stub
+
+}
+
+IniFileData::~IniFileData()
+{
+	// TODO Auto-generated destructor stub
+}
+
+}
