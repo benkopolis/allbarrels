eb3273c3eb581626b42dab4c7f299ef0f16fb1bc
benkopolis <benkopolis@102d4010-3c10-11de-be05-5f7a86268029>
2009-05-28 17:26:19 +0000

    lol

--- AALBarrels/IniFileData.cpp
+++ AALBarrels/IniFileData.cpp
@@ -21,4 +21,142 @@ IniFileData::~IniFileData()
 	// TODO Auto-generated destructor stub
 }
 
+
+char* IniFileData::getIniFileName() const
+{
+    return _iniFileName;
+}
+
+void IniFileData::setIniFileName(char *_iniFileName)
+{
+    this->_iniFileName = _iniFileName;
+}
+
+int IniFileData::getHowMany() const
+{
+    return _HowMany;
+}
+
+char IniFileData::getChangeWay() const
+{
+    return _ChangeWay;
+}
+
+int IniFileData::getChangeVal() const
+{
+    return _ChangeVal;
+}
+
+int IniFileData::getIterCount() const
+{
+    return _IterCount;
+}
+
+bool IniFileData::getSav() const
+{
+    return _Sav;
+}
+
+char* IniFileData::getSavToFile() const
+{
+    return _SavToFile;
+}
+
+bool IniFileData::getLoad() const
+{
+    return _Load;
+}
+
+char IniFileData::*getLoadFromFile() const
+{
+    return _LoadFromFile;
+}
+
+bool IniFileData::getConsole() const
+{
+    return _Console;
+}
+
+bool IniFileData::getGenTable() const
+{
+    return _GenTable;
+}
+
+bool IniFileData::getShowWork() const
+{
+    return _ShowWork;
+}
+
+bool IniFileData::getCheckTimes() const
+{
+    return _CheckTimes;
+}
+
+/**
+ * -------------------------------------------------------------------------------------
+ *  PROTECTED GETTERS AND SETTERS
+ *  ------------------------------------------------------------------------------------
+ */
+
+void IniFileData::setHowMany(int _HowMany)
+{
+     this->_HowMany = _HowMany;
+}
+
+void IniFileData::setChangeVal(int _ChangeVal)
+{
+    this->_ChangeVal = _ChangeVal;
+}
+
+void IniFileData::setChangeWay(char _ChangeWay[4])
+{
+    this->_ChangeWay = _ChangeWay;
+}
+
+void IniFileData::setIterCount(int _IterCount)
+{
+    this->_IterCount = _IterCount;
+}
+
+void IniFileData::setSav(bool _Sav)
+{
+    this->_Sav = _Sav;
+}
+
+void IniFileData::setSavToFile(char *_SavToFile)
+{
+    this->_SavToFile = _SavToFile;
+}
+
+void IniFileData::setLoad(bool _Load)
+{
+    this->_Load = _Load;
+}
+
+void IniFileData::setLoadFromFile(char *_LoadFromFile)
+{
+    this->_LoadFromFile = _LoadFromFile;
+}
+
+void IniFileData::setConsole(bool _Console)
+{
+    this->_Console = _Console;
+}
+
+void IniFileData::setGenTable(bool _GenTable)
+{
+    this->_GenTable = _GenTable;
+}
+
+void IniFileData::setShowWork(bool _ShowWork)
+{
+    this->_ShowWork = _ShowWork;
+}
+
+void IniFileData::setCheckTimes(bool _CheckTimes)
+{
+    this->_CheckTimes = _CheckTimes;
+}
+
+
 }
--- AALBarrels/IniFileData.h
+++ AALBarrels/IniFileData.h
@@ -15,7 +15,147 @@ class IniFileData
 {
 public:
 	IniFileData();
+	IniFileData(char *fileName);
 	virtual ~IniFileData();
+
+	/**
+	 * Metoda pobiera dane z pliku i zapisuje w zmiennych prywatnych klasy.
+	 */
+	void loadData();
+
+	/**
+	 * -------------------------------------------------------------------------------------
+	 *  PUBLIC GETTERS AND SETTERS
+	 *  ------------------------------------------------------------------------------------
+	 */
+
+    char *getIniFileName() const;
+
+    void setIniFileName(char *_iniFileName);
+
+    int getHowMany() const;
+
+    char getChangeWay() const;
+
+    int getChangeVal() const;
+
+    int getIterCount() const;
+
+    bool getSav() const;
+
+    char *getSavToFile() const;
+
+    bool getLoad() const;
+
+    char *getLoadFromFile() const;
+
+    bool getConsole() const;
+
+    bool getGenTable() const;
+
+    bool getShowWork() const;
+
+    bool getCheckTimes() const;
+
+	/**
+	 * -------------------------------------------------------------------------------------
+	 *  PROTECTED GETTERS AND SETTERS
+	 *  ------------------------------------------------------------------------------------
+	 */
+
+protected:
+
+    void setHowMany(int _HowMany);
+
+    void setChangeVal(int _ChangeVal);
+
+    void setChangeWay(char _ChangeWay[4]);
+    void setIterCount(int _IterCount);
+
+    void setSav(bool _Sav);
+
+    void setSavToFile(char *_SavToFile);
+
+    void setLoad(bool _Load);
+
+    void setLoadFromFile(char *_LoadFromFile);
+
+    void setConsole(bool _Console);
+
+    void setGenTable(bool _GenTable);
+
+    void setShowWork(bool _ShowWork);
+
+    void setCheckTimes(bool _CheckTimes);
+
+private:
+
+	char *_iniFileName;
+
+	/**
+	 * jak duzo beczek za pierwszym razem wygenerowac - domyslnie 100
+	 */
+	int _HowMany;
+
+	/**
+	 * Sposób zwiększania ilości danych - może być mult lub add lub non
+	 * domyślnie add
+	 */
+	char _ChangeWay[4];
+
+	/**
+	 * Wartość o jaką dokonywana jest zmiana
+	 * domyślnie 100
+	 */
+	int _ChangeVal;
+
+	/**
+	 * Ilość powtórzeń przebiegów algorytmu - domyślnie jest 10
+	 */
+	int _IterCount;
+
+	/**
+	 * Mówi czy zapisywać dane do pliku. Domyślnie nie, ale jak tak, to nazwę pliku
+	 * bierze ze zmiennej _SavToFile
+	 */
+	bool _Sav;
+
+	/**
+	 * Nazwa pliku do którego trzeba wygenerować dane. Domyślnie dane.dat
+	 */
+	char *_SavToFile;
+
+	/**
+	 * Mówi czy wczytywać dane. Jak tak to nazwe pliku
+	 * z danymi pobieraz ze zmiennej LoadFromFile.
+	 * Domyślnie jest false.
+	 */
+	bool _Load;
+
+	/**
+	 * Nazwa pliku z danymi do załadowania. Domyślnie dane.dat
+	 */
+	char *_LoadFromFile;
+
+	/**
+	 * Mówi czy pobierać dane z konsoli. Domyślnie false.
+	 */
+	bool _Console;
+
+	/**
+	 * mówi czy generować tabelke.
+	 */
+	bool _GenTable;
+
+	/**
+	 * Mówi czy pokazywać przebiegi sortowania.
+	 */
+	bool _ShowWork;
+
+	/**
+	 * Mówi czy mierzyć czasy.
+	 */
+	bool _CheckTimes;
 };
 
 }
